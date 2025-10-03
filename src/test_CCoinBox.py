c5ceaf09b60e6f7b5a223c17c5314226641238dc
Liangchen Liu <104337615+LLiu223@users.noreply.github.com>
2025-10-03 10:20:05 -0400

    Change expected value in vente test from True to False

--- src/test_CCoinBox.py
+++ src/test_CCoinBox.py
@@ -26,4 +26,4 @@ class Test_CCoinBox(unittest.TestCase):
         coinBox.ajouter_25c()
         coinBox.ajouter_25c()
         coinBox.vente()
-        self.assertEqual(coinBox.get_vente_permise(), True)
+        self.assertEqual(coinBox.get_vente_permise(), False)
