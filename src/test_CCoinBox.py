ccaae3cd0669ad19822b14f301a600a28ce175d0
Liangchen Liu <104337615+LLiu223@users.noreply.github.com>
2025-10-03 10:46:26 -0400

    Update test_CCoinBox.py

--- src/test_CCoinBox.py
+++ src/test_CCoinBox.py
@@ -26,4 +26,14 @@ class Test_CCoinBox(unittest.TestCase):
         coinBox.ajouter_25c()
         coinBox.ajouter_25c()
         coinBox.vente()
-        self.assertEqual(coinBox.get_vente_permise(), True)
+        self.assertEqual(coinBox.get_vente_permise(), False)
+
+    def test_trouver_mutant(self):
+        coinBox = CCoinBox()
+        coinBox.ajouter_25c()
+        coinBox.ajouter_25c()
+        coinBox.ajouter_25c()
+        coinBox.ajouter_25c()
+        coinBox.ajouter_25c()
+        coinBox.vente()
+        self.assertEqual(coinBox.get_vente_permise(), False)
